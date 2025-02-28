53eedd2dd4e9c10ba1e4ac4c97ea12d570f41d66
Ciprian <hurmuzache.ciprian@gmail.com>
2025-02-28 15:54:59 +0000

    added pino instrumentation

--- src/payment/opentelemetry.js
+++ src/payment/opentelemetry.js
@@ -5,7 +5,6 @@ const opentelemetry = require("@opentelemetry/sdk-node")
 const {getNodeAutoInstrumentations} = require("@opentelemetry/auto-instrumentations-node")
 const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-grpc')
 const {OTLPMetricExporter} = require('@opentelemetry/exporter-metrics-otlp-grpc')
-const {OTLPLogExporter} = require('@opentelemetry/exporter-logs-otlp-http');
 const {PeriodicExportingMetricReader} = require('@opentelemetry/sdk-metrics')
 const {alibabaCloudEcsDetector} = require('@opentelemetry/resource-detector-alibaba-cloud')
 const {awsEc2Detector, awsEksDetector} = require('@opentelemetry/resource-detector-aws')
@@ -13,11 +12,12 @@ const {containerDetector} = require('@opentelemetry/resource-detector-container'
 const {gcpDetector} = require('@opentelemetry/resource-detector-gcp')
 const {envDetector, hostDetector, osDetector, processDetector} = require('@opentelemetry/resources')
 const {RuntimeNodeInstrumentation} = require('@opentelemetry/instrumentation-runtime-node')
+const { PinoInstrumentation } = require('@opentelemetry/instrumentation-pino');
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter(),
-  logExporter: new OTLPLogExporter(),
   instrumentations: [
+    new PinoInstrumentation(),
     getNodeAutoInstrumentations({
       // only instrument fs if it is part of another trace
       '@opentelemetry/instrumentation-fs': {
