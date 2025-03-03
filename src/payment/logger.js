af562da7df0f6e9f9391a897ac2a17314d9cda64
Karthik K N <karthik.kn@liteverge.com>
2025-03-03 20:47:54 +0000

    payment and checkout logs written to mounted logs folder along with stdout

--- src/payment/logger.js
+++ src/payment/logger.js
@@ -5,15 +5,24 @@ const pino = require('pino')
 
 const logger = pino({
     mixin() {
-      return {
-        'service.name': process.env['OTEL_SERVICE_NAME'],
-      }  
+        return {
+            'service.name': process.env['OTEL_SERVICE_NAME'],
+        }
     },
-    formatters: {
-        level: (label) => {
-          return { 'level': label };
-        },
-      },
+    transport: {
+        targets: [
+            {
+                target: 'pino/file',
+                options: {destination: '/logs/application.log', mkdir: false},
+                level: 'info',
+            },
+            {
+                target: 'pino/file',
+                options: {destination: 1}, // stdout
+                level: 'info', // explicitly set level if required
+            },
+        ],
+    },
 });
 
 module.exports = logger;
